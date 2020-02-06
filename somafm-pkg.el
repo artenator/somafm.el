af164b706ab501a3d2451568452b22a654c1e6a3
Arte Ebrahimi <arteebrahimi@gmail.com>
2020-02-06 14:11:02 -0800

    fix package linting errors

---
+++ somafm-pkg.el
@@ -0,0 +1,8 @@
+(define-package
+  "somafm"
+  "0.1.0"
+  "A simple soma.fm interface"
+  '((emacs "26.1")
+    (dash "2.12.0")
+    (request "0.3.2")
+    (cl-lib "0.6.1")))
--- somafm.el
+++ somafm.el
@@ -1,4 +1,24 @@
-;;; somafm.el --- A soma.fm interface for emacs -*- lexical-binding: t -*-
+;;; somafm.el --- A simple soma.fm interface -*- lexical-binding: t -*-
+
+;; Author: Arte Ebrahimi <>
+;; Keywords: multimedia
+;; Package-Requires: ((emacs "26.1") (dash "2.12.0") (request "0.3.2") (cl-lib "0.6.1"))
+;; URL: https://github.com/artenator/somafm.el
+;; Version: 0.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; somafm.el brings a simple user interface for soma.fm to Emacs.
 
